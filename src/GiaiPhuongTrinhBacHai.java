01089f95ca90f20db6554282d3c101db9c61e7a6
nggiabao2004 <nggiabao2004@icloud.com>
2024-05-23 09:56:42 +0700

    first commit

---
+++ src/GiaiPhuongTrinhBacHai.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class GiaiPhuongTrinhBacHai {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = 2;
+
+        double b = 2;
+
+        double c = a * b;
+
+        System.out.println(c);
+        
+    }
+}
