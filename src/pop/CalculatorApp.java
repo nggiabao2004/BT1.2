ba1aa88a1604e13e206a7e5545a6abc2bf308865
nggiabao2004 <nggiabao2004@icloud.com>
2024-05-23 10:57:39 +0700

    pop và opp

--- src/pop/CalculatorApp.java
+++ src/pop/CalculatorApp.java
@@ -4,10 +4,16 @@ public class CalculatorApp {
 
     public static void main(String[] args)  //Begin
     {
+        int c= multiply();
+        System.out.println("Ket Qua: "+ c);
+        return; //Return: Tra quyen dieu khien cho he dieu hanh
+    }    //End
+
+    static int multiply()  //Begin ham
+    {
         int a= 2;
         int b= 2;
         int c= a*b;
-        System.out.println("Ket Qua: "+ c);
-    return; //Return: Tra quyen dieu khien cho he dieu hanh
-    }    //End
+        return c;   //Tra lai gia tri c ve ban dau
+    }   //End ham
 }
